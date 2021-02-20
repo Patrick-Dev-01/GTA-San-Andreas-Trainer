02d0cf461578a9cd83b9a24077c77229a89298ee
Patrick <patrick.rdecarvalho@outlook.com>
2021-02-20 03:21:25 -0300

    feature: Primeiro commit

---
+++ Trainer-GTA-San-Andreas/Trainer.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Trainer_GTA_San_Andreas
+{
+    partial class Trainer
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Trainer));
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.MaxMoney = new System.Windows.Forms.CheckBox();
+            this.InfiniteAmmo = new System.Windows.Forms.CheckBox();
+            this.godmodeCheckbox = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ArmorRecoveryButton = new System.Windows.Forms.Button();
+            this.recoveryHeatlhButton = new System.Windows.Forms.Button();
+            this.hitmanLevelsCheckbox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.WorkerReportsProgress = true;
+            this.BgWorker.WorkerSupportsCancellation = true;
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BG_Worker_DoWork);
+            this.BgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgWorker_ProgressChanged);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // MaxMoney
+            // 
+            this.MaxMoney.AutoSize = true;
+            this.MaxMoney.Location = new System.Drawing.Point(432, 31);
+            this.MaxMoney.Name = "MaxMoney";
+            this.MaxMoney.Size = new System.Drawing.Size(101, 21);
+            this.MaxMoney.TabIndex = 0;
+            this.MaxMoney.Text = "Max money";
+            this.MaxMoney.UseVisualStyleBackColor = true;
+            this.MaxMoney.CheckStateChanged += new System.EventHandler(this.MaxMoney_CheckStateChanged);
+            // 
+            // InfiniteAmmo
+            // 
+            this.InfiniteAmmo.AutoSize = true;
+            this.InfiniteAmmo.Location = new System.Drawing.Point(432, 58);
+            this.InfiniteAmmo.Name = "InfiniteAmmo";
+            this.InfiniteAmmo.Size = new System.Drawing.Size(114, 21);
+            this.InfiniteAmmo.TabIndex = 1;
+            this.InfiniteAmmo.Text = "Infinite Ammo";
+            this.InfiniteAmmo.UseVisualStyleBackColor = true;
+            this.InfiniteAmmo.CheckStateChanged += new System.EventHandler(this.InfiniteAmmo_CheckStateChanged);
+            // 
+            // godmodeCheckbox
+            // 
+            this.godmodeCheckbox.AutoSize = true;
+            this.godmodeCheckbox.Location = new System.Drawing.Point(432, 85);
+            this.godmodeCheckbox.Name = "godmodeCheckbox";
+            this.godmodeCheckbox.Size = new System.Drawing.Size(96, 21);
+            this.godmodeCheckbox.TabIndex = 5;
+            this.godmodeCheckbox.Text = "God Mode";
+            this.godmodeCheckbox.UseVisualStyleBackColor = true;
+            this.godmodeCheckbox.CheckStateChanged += new System.EventHandler(this.godmodeCheckbox_CheckStateChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(393, 445);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ArmorRecoveryButton
+            // 
+            this.ArmorRecoveryButton.Location = new System.Drawing.Point(669, 163);
+            this.ArmorRecoveryButton.Name = "ArmorRecoveryButton";
+            this.ArmorRecoveryButton.Size = new System.Drawing.Size(101, 31);
+            this.ArmorRecoveryButton.TabIndex = 7;
+            this.ArmorRecoveryButton.Text = "Armor";
+            this.ArmorRecoveryButton.UseVisualStyleBackColor = true;
+            this.ArmorRecoveryButton.Click += new System.EventHandler(this.ArmorRecoveryButton_Click);
+            // 
+            // recoveryHeatlhButton
+            // 
+            this.recoveryHeatlhButton.Location = new System.Drawing.Point(432, 163);
+            this.recoveryHeatlhButton.Name = "recoveryHeatlhButton";
+            this.recoveryHeatlhButton.Size = new System.Drawing.Size(96, 31);
+            this.recoveryHeatlhButton.TabIndex = 8;
+            this.recoveryHeatlhButton.Text = "Heatlh";
+            this.recoveryHeatlhButton.UseVisualStyleBackColor = true;
+            this.recoveryHeatlhButton.Click += new System.EventHandler(this.recoveryHeatlhButton_Click);
+            // 
+            // hitmanLevelsCheckbox
+            // 
+            this.hitmanLevelsCheckbox.AutoSize = true;
+            this.hitmanLevelsCheckbox.Location = new System.Drawing.Point(432, 112);
+            this.hitmanLevelsCheckbox.Name = "hitmanLevelsCheckbox";
+            this.hitmanLevelsCheckbox.Size = new System.Drawing.Size(133, 21);
+            this.hitmanLevelsCheckbox.TabIndex = 9;
+            this.hitmanLevelsCheckbox.Text = "All Hitman levels";
+            this.hitmanLevelsCheckbox.UseVisualStyleBackColor = true;
+            this.hitmanLevelsCheckbox.CheckedChanged += new System.EventHandler(this.hitmanLevelsCheckbox_CheckedChanged);
+            // 
+            // Trainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 469);
+            this.Controls.Add(this.hitmanLevelsCheckbox);
+            this.Controls.Add(this.recoveryHeatlhButton);
+            this.Controls.Add(this.ArmorRecoveryButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.godmodeCheckbox);
+            this.Controls.Add(this.InfiniteAmmo);
+            this.Controls.Add(this.MaxMoney);
+            this.Name = "Trainer";
+            this.Text = "GTA San Andreas Trainer";
+            this.Load += new System.EventHandler(this.Trainer_Load);
+            this.Shown += new System.EventHandler(this.Trainer_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.CheckBox MaxMoney;
+        private System.Windows.Forms.CheckBox InfiniteAmmo;
+        private System.Windows.Forms.CheckBox godmodeCheckbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ArmorRecoveryButton;
+        private System.Windows.Forms.Button recoveryHeatlhButton;
+        private System.Windows.Forms.CheckBox hitmanLevelsCheckbox;
+    }
+}
+
---
+++ Trainer-GTA-San-Andreas/Trainer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Threading;
+
+namespace Trainer_GTA_San_Andreas
+{
+    public partial class Trainer : Form
+    {
+        public Mem m = new Mem();
+        public Trainer()
+        {
+            InitializeComponent();
+        }
+
+        private void Trainer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool ProcessOpen = false;
+
+        private void BG_Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // impedi o uso excessivo da CPU
+            while (true)
+            {
+                ProcessOpen = m.OpenProcess("gta-sa.exe");
+                // Verificar se  o Resident evil 4 esta executando
+                if (ProcessOpen)
+                {
+                    Thread.Sleep(1000);
+                    return;
+                }
+
+                Thread.Sleep(1000);
+                BgWorker.ReportProgress(0);
+            }
+        }
+        private void Trainer_Shown(object sender, EventArgs e)
+        {
+            // assim que o trainer carregar ele inicia o background worker
+            BgWorker.RunWorkerAsync();
+        }
+
+        private void BgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // assim que o trainer carregar ele inicia o background worker
+            BgWorker.RunWorkerAsync();
+        }
+
+        private void MaxMoney_CheckStateChanged(object sender, EventArgs e)
+        {
+            m.WriteMemory("gta-sa.exe+0x00FD7C,B8", "int", "999999999");
+            Thread.Sleep(100);
+        }
+
+        private void InfiniteAmmo_CheckStateChanged(object sender, EventArgs e)
+        {
+            string[] weaponsAmmoAdress = { "gta-sa.exe+0x7FC7D8,5E4", "gta-sa.exe+0x7FC7D8,600", "gta-sa.exe+0x7FC7D8,61C", "gta-sa.exe+0x00802ECC,AC" };
+
+            if (InfiniteAmmo.Checked)
+            {
+                for (int i = 0; i < weaponsAmmoAdress.Length; i++) {
+                    m.FreezeValue(weaponsAmmoAdress[i], "int", "999");
+                }
+            }
+
+            else {
+                for (int i = 0; i < weaponsAmmoAdress.Length; i++)
+                {
+                    m.UnfreezeValue(weaponsAmmoAdress[i]);
+                }
+            }
+          
+            Thread.Sleep(100);
+        }
+  
+        private void godmodeCheckbox_CheckStateChanged(object sender, EventArgs e)
+        {
+            string lifeAdress = "gta-sa.exe+0x007FC7D8,540";
+            string armorAdress = "gta-sa.exe+0x007FC7D8,548";
+
+            if (godmodeCheckbox.Checked)
+            {
+                m.FreezeValue(lifeAdress, "float", "100");
+                m.FreezeValue(armorAdress, "float", "100");
+            }
+
+            else {
+                m.UnfreezeValue(lifeAdress);
+                m.UnfreezeValue(armorAdress);
+            }
+
+            Thread.Sleep(100);
+        }
+
+        private void recoveryHeatlhButton_Click(object sender, EventArgs e)
+        {
+            m.WriteMemory("gta-sa.exe+0x007FC7D8,540", "float", "100");
+            Thread.Sleep(100);
+        }
+
+        private void ArmorRecoveryButton_Click(object sender, EventArgs e)
+        {
+            m.WriteMemory("gta-sa.exe+0x007FC7D8,548", "float", "100");
+            Thread.Sleep(100);
+        }
+
+        private void hitmanLevelsCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            string[] weaponSkilsAdress = { "gta-sa.exe+0x00172664,4", "gta-sa.exe+0x008B91E0,1A4,31C", "gta-sa.exe+0x00172664,1C", "gta-sa.exe+0x0017279C,2C"};
+
+            for (int i = 0; i < weaponSkilsAdress.Length; i++) {
+                m.WriteMemory(weaponSkilsAdress[i], "float", "1000");
+            }
+        }
+    }
+}
